ee1e46bce442935e341430aa8384c2cc79121fb6
wangruihuan <1234@qq.com>
2019-08-04 20:06:13 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 产品级别提示  不开启
 Vue.config.productionTip = false
 
+// main.js 入口文件
+//  职责.1 引入需要依赖的模块
+//       2.创建一个根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
